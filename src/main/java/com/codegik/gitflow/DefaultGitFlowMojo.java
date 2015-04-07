d7d126db278c468c42a7cb43711f7c9f22f77229
Inacio G Klassmann <inaciosk8@gmail.com>
2015-04-06 21:40:55 -0300

    Faltou comitar

---
+++ src/main/java/com/codegik/gitflow/DefaultGitFlowMojo.java
@@ -0,0 +1,166 @@
+package com.codegik.gitflow;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.maven.shared.release.config.ReleaseDescriptor;
+import org.apache.maven.shared.release.env.DefaultReleaseEnvironment;
+import org.apache.maven.shared.release.env.ReleaseEnvironment;
+import org.eclipse.jgit.api.ListBranchCommand.ListMode;
+import org.eclipse.jgit.api.ResetCommand.ResetType;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.MergeResult;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.merge.MergeStrategy;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
+
+public abstract class DefaultGitFlowMojo extends AbstractGitFlowMojo {
+	private Git git;
+
+
+	protected Git getGit() throws Exception {
+		if (git == null) {
+			git = Git.open(new File("."));
+		}
+
+		return git;
+	}
+
+
+	protected MergeResult merge(Ref ref) throws Exception {
+		return merge(ref, null);
+	}
+
+
+	protected MergeResult merge(Ref ref, MergeStrategy mergeStrategy) throws Exception {
+		getLog().info("Merging " + ref.getName() + " into " + getGit().getRepository().getBranch());
+
+		if (mergeStrategy == null) {
+			return getGit().merge().include(ref).call();
+		}
+
+		return getGit().merge().setStrategy(mergeStrategy).include(ref).call();
+	}
+
+
+	protected List<String> deleteBranch(String branchName) throws Exception {
+		getLog().info("Deleting branch " + branchName);
+		return getGit().branchDelete().setForce(true).setBranchNames(branchName).call();
+	}
+
+
+	protected List<String> deleteTag(String tagName) throws Exception {
+		getLog().info("Deleting tag " + tagName);
+		return getGit().tagDelete().setTags(tagName).call();
+	}
+
+
+	protected Ref reset(String branchName) throws Exception {
+		getLog().info("Reseting into " + branchName);
+		return getGit().reset().setMode(ResetType.HARD).setRef(branchName).call();
+	}
+
+
+	protected Ref checkoutBranchForced(String branchName) throws Exception {
+		getLog().info("Checkout forced into " + branchName);
+		return getGit().checkout().setCreateBranch(false).setForce(true).setName(branchName).call();
+	}
+
+
+	protected Ref checkoutBranch(String branchName) throws Exception {
+		getLog().info("Checkout into " + branchName);
+		return getGit().checkout().setCreateBranch(false).setName(branchName).call();
+	}
+
+
+	protected Ref createBranch(String branchName) throws Exception {
+		return createBranch(branchName, null);
+	}
+
+
+	protected Ref createBranch(String branchName, String logMessage) throws Exception {
+		getLog().info("Creating branch " + branchName + (logMessage == null ? "" : " " + logMessage));
+		return getGit().checkout().setCreateBranch(true).setName(branchName).call();
+	}
+
+
+	protected RevCommit commit(String message) throws Exception {
+		return getGit().commit().setAll(true).setMessage(message).call();
+	}
+
+
+	protected Iterable<PushResult> push() throws Exception {
+		return push(null);
+	}
+
+
+	protected Iterable<PushResult> push(String logMessage) throws Exception {
+		getLog().info(logMessage == null ? "Pushing commit" : logMessage);
+
+		if (getUsername() != null && getPassword() != null) {
+	        CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(getUsername(), getPassword());
+	        return getGit().push().setCredentialsProvider(credentialsProvider).call();
+		}
+
+		return getGit().push().call();
+	}
+
+
+	protected Ref findBranch(String branch) throws Exception {
+		getLog().info("Looking for branch " + branch);
+
+		for (Ref b : getGit().branchList().setListMode(ListMode.ALL).call()) {
+			if (branch.equals(b.getName().toLowerCase().replace("refs/heads/", "")) ||
+				branch.equals(b.getName().toLowerCase().replace("refs/remotes/origin/", ""))) {
+				return b;
+			}
+		}
+
+		return null;
+	}
+
+
+	/**
+	 * TODO
+	 * Nao funciona!
+	 */
+	protected Ref findTag(String tag) throws Exception {
+		getLog().info("Looking for tag " + tag);
+
+		for (Ref b : getGit().tagList().call()) {
+			if (tag.equals(b.getName().toLowerCase().replace("refs/tags/", ""))) {
+				return b;
+			}
+		}
+
+		return null;
+	}
+
+
+	protected ReleaseEnvironment buildDefaultReleaseEnvironment() throws Exception {
+		ReleaseEnvironment environment = new DefaultReleaseEnvironment();
+
+		environment.setLocalRepositoryDirectory(getGit().getRepository().getDirectory());
+
+		return environment;
+	}
+
+
+	protected ReleaseDescriptor buildReleaseDescriptor() throws Exception {
+		ReleaseDescriptor descriptor = new ReleaseDescriptor();
+
+		descriptor.setAutoVersionSubmodules(true);
+		descriptor.setInteractive(false);
+		descriptor.setUpdateWorkingCopyVersions(true);
+		descriptor.setWorkingDirectory(getGit().getRepository().getDirectory().getParent());
+		descriptor.setScmUsername(getUsername());
+		descriptor.setScmPassword(getPassword());
+		descriptor.setUpdateDependencies(true);
+
+		return descriptor;
+	}
+}
---
+++ src/main/java/com/codegik/gitflow/mojo/FinishReleaseMojo.java
@@ -0,0 +1,97 @@
+package com.codegik.gitflow.mojo;
+
+import java.util.List;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.release.ReleaseCleanRequest;
+import org.apache.maven.shared.release.config.ReleaseDescriptor;
+import org.apache.maven.shared.release.env.ReleaseEnvironment;
+import org.eclipse.jgit.api.MergeResult;
+import org.eclipse.jgit.lib.Ref;
+
+import com.codegik.gitflow.DefaultGitFlowMojo;
+
+
+/**
+ * Start new release branch from develop
+ *
+ * @author Inacio G Klassmann
+ */
+@Mojo(name = "finish-release", aggregator = true)
+public class FinishReleaseMojo extends DefaultGitFlowMojo {
+	private Ref lastTag;
+
+    @Parameter( property = "version", required = true )
+	private String version;
+
+
+	@Override
+	public void run() throws Exception {
+		validadeVersion(getVersion());
+
+		checkoutBranch(DEVELOP);
+
+		Ref ref = findBranch(PREFIX_RELEASE + SEPARATOR + getVersion());
+		MergeResult merge = merge(ref);
+
+		if (!merge.getMergeStatus().isSuccessful()) {
+			getLog().error("There is conflicts in the following files:");
+			for (String key : merge.getConflicts().keySet()) {
+				getLog().error(key);
+			}
+			String message = "\nThe merge has conflicts, please try resolve manually! [from " + ref.getName() + " to " + DEVELOP + "]";
+			message += "\nExecute the steps:";
+			message += "\ngit reset --hard " + DEVELOP;
+			message += "\ngit checkout " + DEVELOP;
+			message += "\ngit merge " + ref.getName();
+			message += "\nmvn gitflow:finish-release -Dversion=" + getVersion();
+			throw buildMojoException(message);
+		}
+
+		getLog().info("Updating pom version");
+		ReleaseDescriptor descriptor 	= buildReleaseDescriptor();
+		ReleaseCleanRequest clean		= new ReleaseCleanRequest();
+		ReleaseEnvironment environment 	= buildDefaultReleaseEnvironment();
+		List<MavenProject> projects		= buildMavenProjects();
+
+		clean.setReactorProjects(projects);
+		clean.setReleaseDescriptor(descriptor);
+
+		getReleaseManager().prepare(descriptor, environment, projects);
+		getReleaseManager().clean(clean);
+
+		lastTag = findTag(getGit().describe().call());
+
+		getLog().info("Commiting changed files");
+		commit("[GitFlow::finishi-release] Finish release branch " + getVersion());
+
+		push("Pushing commit");
+	}
+
+
+	@Override
+	public void rollback(Exception e) throws MojoExecutionException {
+		try {
+			getLog().error(e.getMessage());
+			getLog().info("Rolling back all changes");
+			reset(DEVELOP);
+			checkoutBranchForced(DEVELOP);
+			deleteTag(lastTag.getName());
+		} catch (Exception e1) {;}
+		throw buildMojoException("ERROR", e);
+	}
+
+
+	public String getVersion() {
+		return version;
+	}
+
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+}
